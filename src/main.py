2becb676d51d6bcd9b66f12e20bffe3e6543e6af
icrocs <tr4shs3ri0@proton.me>
2026-04-14 10:53:19 +0200

    Bozza main e solvers

---
+++ src/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import utility
+import solvers
+
+
+def main(argv, argc):
+    if argc < 3:
+        print(f'Usage: {argv[0]} <mmx file> <tolerance>')
+        return -1
+
+    # Check if tolerance is integer
+    try:
+        tolerance = float(argv[2])
+    except ValueError:
+        print('Specify float value for tolerance')
+        return -1
+
+    #m = mmread(argv[1], spmatrix=True)
+    #array_mat = m.toarray()
+
+    m = utility.load_matrix(argv[1]).toarray()
+
+    solver = solvers.MatSolvers(m, tolerance)
+
+    #TODO For each solver method print result
+    
+
+if __name__ == '__main__':
+    main(sys.argv, len(sys.argv))
---
+++ src/solvers.py
@@ -0,0 +1,17 @@
+class MatSolvers():
+    def __init__(self, mat, tol, max_iter=5000):
+        self.mat = mat
+        self.tolerance = tol
+        self.max_iter = max_iter
+
+    def jacobi(self):
+        return 0
+
+    def gauss_seidel(self):
+        return 0
+
+    def gradiente(self):
+        return 0
+
+    def gradiente_coniugato(self):
+        return 0
